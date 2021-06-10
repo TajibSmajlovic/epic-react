e574ad6e1dda1bbfd356cb7f19e68aa27fdad46c
TajibSmajlovic <smajlovic.tajib@gmail.com>
2021-06-10 12:13:19 +0200

    adding 05_react-suspense

---
+++ 05_react-suspense/src/exercise/04.js
@@ -0,0 +1,137 @@
+// Cache resources
+// http://localhost:3000/isolated/exercise/04.js
+
+import * as React from 'react'
+import {
+  fetchPokemon,
+  PokemonInfoFallback,
+  PokemonForm,
+  PokemonDataView,
+  PokemonErrorBoundary,
+} from '../pokemon'
+import {createResource} from '../utils'
+
+function PokemonInfo({pokemonResource}) {
+  const pokemon = pokemonResource.read()
+  return (
+    <div>
+      <div className="pokemon-info__img-wrapper">
+        <img src={pokemon.image} alt={pokemon.name} />
+      </div>
+      <PokemonDataView pokemon={pokemon} />
+    </div>
+  )
+}
+
+const SUSPENSE_CONFIG = {
+  timeoutMs: 4000,
+  busyDelayMs: 300,
+  busyMinDurationMs: 700,
+}
+
+const PokemonResourceCacheContext = React.createContext()
+
+function PokemonCacheProvided({cacheTime, children}) {
+  const cache = React.useRef({})
+  const expirations = React.useRef({})
+
+  React.useEffect(() => {
+    const interval = setInterval(() => {
+      for (const [name, time] of Object.entries(expirations.current)) {
+        if (time < Date.now()) {
+          delete cache.current[name]
+          delete expirations.current[name]
+        }
+      }
+    }, 1000)
+
+    return () => {
+      clearInterval(interval)
+    }
+  }, [])
+
+  const getPokemonResource = React.useCallback(
+    pokemonName => {
+      const name = pokemonName.toLowerCase()
+      let resource = cache.current[name]
+
+      if (!resource) {
+        resource = createResource(fetchPokemon(name))
+        cache.current[name] = resource
+      }
+
+      expirations.current[name] = Date.now() + cacheTime
+
+      return resource
+    },
+    [cacheTime],
+  )
+
+  return (
+    <PokemonResourceCacheContext.Provider value={getPokemonResource}>
+      {children}
+    </PokemonResourceCacheContext.Provider>
+  )
+}
+
+function usePokemonResourceCache() {
+  return React.useContext(PokemonResourceCacheContext)
+}
+
+function App() {
+  const [pokemonName, setPokemonName] = React.useState('')
+  const [startTransition, isPending] = React.useTransition(SUSPENSE_CONFIG)
+  const [pokemonResource, setPokemonResource] = React.useState(null)
+  const getPokemonResource = usePokemonResourceCache()
+
+  React.useEffect(() => {
+    if (!pokemonName) {
+      setPokemonResource(null)
+      return
+    }
+    startTransition(() => {
+      setPokemonResource(getPokemonResource(pokemonName))
+    })
+  }, [getPokemonResource, pokemonName, startTransition])
+
+  function handleSubmit(newPokemonName) {
+    setPokemonName(newPokemonName)
+  }
+
+  function handleReset() {
+    setPokemonName('')
+  }
+
+  return (
+    <div className="pokemon-info-app">
+      <PokemonForm pokemonName={pokemonName} onSubmit={handleSubmit} />
+      <hr />
+      <div className={`pokemon-info ${isPending ? 'pokemon-loading' : ''}`}>
+        {pokemonResource ? (
+          <PokemonErrorBoundary
+            onReset={handleReset}
+            resetKeys={[pokemonResource]}
+          >
+            <React.Suspense
+              fallback={<PokemonInfoFallback name={pokemonName} />}
+            >
+              <PokemonInfo pokemonResource={pokemonResource} />
+            </React.Suspense>
+          </PokemonErrorBoundary>
+        ) : (
+          'Submit a pokemon'
+        )}
+      </div>
+    </div>
+  )
+}
+
+function AppWithProvider() {
+  return (
+    <PokemonCacheProvided cacheTime={5000}>
+      <App />
+    </PokemonCacheProvided>
+  )
+}
+
+export default AppWithProvider
